3d20a552f5a7bdbc1836b16ef32bc490dd38b408
Dennis den Ouden-van der Horst <49307154+douden@users.noreply.github.com>
2026-05-13 11:00:00 +0200

    Add teachbooks_fetch extension to setup

--- src/teachbooks_favourites/__init__.py
+++ src/teachbooks_favourites/__init__.py
@@ -32,6 +32,7 @@ def setup(app: Sphinx) -> Dict[str, Any]:
     app.setup_extension("teachbooks_zoomies")
     app.setup_extension("teachbooks_questions")
     app.setup_extension("sphinx_sticky_margin")
+    app.setup_extension("teachbooks_fetch")
     
     return {
         "version": "builtin",
